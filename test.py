17dc241e621e778f4a846be4d0545355f72f578e
Антон Денисович Букашкин <87475858+ADBuk@users.noreply.github.com>
2023-11-12 22:26:24 +0300

    minor fixes

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from flask_api import app
+from main import app
 
 client = TestClient(app)
 
@@ -7,11 +7,11 @@ client = TestClient(app)
 def test_train_model():
     hyperparameters = {"fit_intercept": True, "copy_X": True}
     training_data = {
-        "features": [[1.0, 2.0], [2.0, 3.0], [3.0, 4.0]],
-        "labels": [2.5, 3.5, 4.5],
+        "features": [[1.0, 2.0], [2.0, 3.0], [3.0, 4.0], [5.0, 6.0]],
+        "labels": [2.5, 3.5, 4.5, 5.5],
     }
     response = client.post(
-        "http://localhost:8000/train_models",
+        "http://0.0.0.0:8000/train_models",
         json={
             "hyperparameters": hyperparameters,
             "training_data": training_data,
@@ -20,8 +20,37 @@ def test_train_model():
         },
     )
     print(response.status_code)
-    assert response.status_code == 200
     data = response.json()
 
+    return data
 
-test_train_model()
+
+print(test_train_model())
+
+
+def test_get_model():
+    response = client.get("http://0.0.0.0:8000/get_all_models")
+    print(response.status_code)
+    data = response.json()
+
+    return data
+
+
+print(test_get_model())
+
+
+def delete_model(model_name="LinReg"):
+    response = client.post(
+        f"/delete_model",
+        json={
+            "model_name": f"{model_name}.pkl",
+        },
+    )
+
+    print(response.status_code)
+    data = response.json()
+
+    return data
+
+
+print(delete_model())
